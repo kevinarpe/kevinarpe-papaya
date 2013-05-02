f85d304998004a6817a378730ecf0afa207991b1
Kevin Connor ARPE <kevinarpe@gmail.com>
2013-05-02 23:57:31 +0800

    Fixed annotations to allow package-level

--- src/main/java/com/googlecode/kevinarpe/papaya/annotations/FullyTested.java
+++ src/main/java/com/googlecode/kevinarpe/papaya/annotations/FullyTested.java
@@ -1,5 +1,30 @@
 package com.googlecode.kevinarpe.papaya.annotations;
 
+/*
+ * #%L
+ * This file is part of Papaya.
+ * %%
+ * Copyright (C) 2013 Kevin Connor ARPE (kevinarpe@gmail.com)
+ * %%
+ * Papaya is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * GPL Classpath Exception:
+ * This project is subject to the "Classpath" exception as provided in
+ * the LICENSE file that accompanied this code.
+ * 
+ * Papaya is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Papaya.  If not, see <http://www.gnu.org/licenses/>.
+ * #L%
+ */
+
 import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
@@ -16,6 +41,7 @@ import java.lang.annotation.Target;
  */
 @Retention(RetentionPolicy.CLASS)
 @Target({
+	ElementType.PACKAGE,
 	ElementType.CONSTRUCTOR,
 	ElementType.METHOD,
 	ElementType.TYPE})
