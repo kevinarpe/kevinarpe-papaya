134eba79747a790d805d7371ea475b3618dd1ceb
Kevin Connor ARPE <kevinarpe@gmail.com>
2014-06-29 22:56:48 +0800

    Massive re-org to collapse some sub-modules into main module Otherwise: No code changes

--- kevinarpe-papaya-jdk-derived/src/test/java/com/googlecode/kevinarpe/papaya/jdk/properties/JdkPropertyTest.java
+++ kevinarpe-papaya-jdk-derived/src/test/java/com/googlecode/kevinarpe/papaya/jdk/properties/JdkPropertyTest.java
@@ -1,5 +1,33 @@
 package com.googlecode.kevinarpe.papaya.jdk.properties;
 
+/*
+ * #%L
+ * This file is part of Papaya (JDK Derived Classes).
+ * %%
+ * Copyright (C) 2013 - 2014 Kevin Connor ARPE (kevinarpe@gmail.com)
+ * %%
+ * Papaya (JDK Derived Classes) is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License version 2 only as
+ * published by the Free Software Foundation.
+ * 
+ * GPL Classpath Exception:
+ * This project is subject to the "Classpath" exception as provided in the LICENSE
+ * file that accompanied this code.
+ * 
+ * Papaya is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * Papaya.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * Special notice for this module of Papaya:
+ * Classes in this module may contain significant portions that are originally
+ * part of the JDK source base.  In such cases, prominent notices appear before
+ * these blocks of source code.
+ * #L%
+ */
+
 import com.google.common.testing.EqualsTester;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
